d98796d92ab01d93dc47074ed6155d399a6cbb55
Skunal07 <skunal@teqmavens.com>
2023-03-02 17:08:53 +0530

    listting

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Controller;
@@ -16,12 +17,17 @@ class CategoriesController extends AppController
      *
      * @return \Cake\Http\Response|null|void Renders view
      */
+    public function beforeFilter($event)
+    {
+        parent::beforeFilter($event);
+        $this->viewBuilder()->setLayout("dashboard");
+    }
     public function index()
     {
         $this->paginate = [
-            'contain' => ['Users'],
+            'contain' => ['Users.UserProfile'],
         ];
-        $categories = $this->paginate($this->Categories);
+        $categories = $this->paginate($this->Categories->find('all')->where(['Categories.status' => 0]));
 
         $this->set(compact('categories'));
     }
@@ -47,20 +53,32 @@ class CategoriesController extends AppController
      *
      * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
      */
-    public function add()
+    public function addcategory()
     {
+        $user = $this->Authentication->getIdentity();
+        $uid=$user->id ;
         $category = $this->Categories->newEmptyEntity();
-        if ($this->request->is('post')) {
+        if ($this->request->is('ajax')) {
             $category = $this->Categories->patchEntity($category, $this->request->getData());
+            $category->user_id=$uid;
             if ($this->Categories->save($category)) {
                 $this->Flash->success(__('The category has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
+                echo json_encode(array(
+                    "status" => 1,
+                    "message" => "category name has been created"
+                ));
+                die;
             }
-            $this->Flash->error(__('The category could not be saved. Please, try again.'));
+
+            $this->Flash->error(__('Failed to save category name'));
+
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "Failed to create"
+            ));
+            die;
         }
-        $users = $this->Categories->Users->find('list', ['limit' => 200])->all();
-        $this->set(compact('category', 'users'));
     }
 
     /**

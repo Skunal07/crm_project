4667b625e144871a5455f6607108a9f3adb41f94
Skunal07 <skunal@teqmavens.com>
2023-03-13 18:33:16 +0530

    **

--- src/Model/Table/LeadsTable.php
+++ src/Model/Table/LeadsTable.php
@@ -49,10 +49,7 @@ class LeadsTable extends Table
             'foreignKey' => 'user_id',
             'joinType' => 'INNER',
         ]);
-        $this->belongsTo('Companies', [
-            'foreignKey' => 'company_id',
-            'joinType' => 'INNER',
-        ]);
+        
         $this->hasOne('LeadContacts', [
             'foreignKey' => 'lead_id',
         ]);

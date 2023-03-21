a2742f9ca4c9411c97336735053233b81702dfd3
[Kuldeep-Teq] <[sokuldeep@teqmavens.com]>
2023-03-21 18:16:21 +0530

    download pdf

--- templates/element/main/main_nav.php
+++ templates/element/main/main_nav.php
@@ -8,13 +8,16 @@
         <div class="collapse navbar-collapse topnav-right" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto float-end">
                 <li class="nav-item active">
-                    <a class="nav-link text-light nav-menu px-3 mx-3 active" href="/users">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link text-light nav-menu px-3 mx-3 " href="/users">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-light nav-menu px-3 mx-3" href="#">About Us</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-light nav-menu px-3 mx-3" href="#">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light nav-menu px-3 mx-3" href="/users/downloadPdf">Download Pdf</a>
                 </li>
             </ul>
             <?= $this->Html->link("Login", ['controller' => 'Users', 'action' => 'login'], ['type' => 'button', 'class' => 'btn btn-light text-dark fw-bold py-1 mx-3']) ?>

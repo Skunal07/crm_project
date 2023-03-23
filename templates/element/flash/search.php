104d79bb190cf4c8a97f4f5d9257a3b4fbbfbe29
[Kuldeep-Teq] <[sokuldeep@teqmavens.com]>
2023-03-23 09:59:10 +0530

    set view button

--- templates/element/flash/search.php
+++ templates/element/flash/search.php
@@ -3,8 +3,8 @@ if ($countall != null) {
     $i = 0;
     $j = 0;
     $k = 0;
-    
-    
+
+
     foreach ($countall['user'] as $user) {
         if ($user != null) {
             if ($i == 0) {
@@ -12,7 +12,7 @@ if ($countall != null) {
                 echo '<hr class="my-0">';
             }
             $i++;
-            echo '<li class="nav-link py-1">' . '<a href="/users/usersList?key='.$user->first_name.'">' . $user->first_name . '</a>' . '</li>';
+            echo '<li class="nav-link py-1">' . '<a href="/users/usersList?key=' . $user->first_name . '">' . $user->first_name . '</a>' . '</li>';
         } else {
             echo '';
         }
@@ -25,11 +25,11 @@ if ($countall != null) {
                 echo '<hr class="my-0">';
             }
             $j++;
-            echo '<li class="nav-link py-1">' . '<a href="/leads/index?key='.$lead->name.'">'. $lead->name . '</a></li>';
+            echo '<li class="nav-link py-1">' . '<a href="/leads/index?key=' . $lead->name . '">' . $lead->name . '</a></li>';
         } else {
             echo '';
         }
-        }
+    }
 
     foreach ($countall['product'] as $product) {
         if ($product != null) {
@@ -38,20 +38,16 @@ if ($countall != null) {
                 echo '<hr class="my-0">';
             }
             $k++;
-                    echo '<li class="nav-link py-1">' . '<a href="/products/index?key='.$product->product_name.'">'. $product->product_name . '</a></li>';
-                } else {
-                    echo '';
-                }
-            }
-            
-       if ($i==0 && $j==0 && $k==0) {
-        echo '<li class="nav-link">' . '<h6 class="text-white">No Data Found<h6></li>';
+            echo '<li class="nav-link py-1">' . '<a href="/products/index?key=' . $product->product_name . '">' . $product->product_name . '</a></li>';
+        } else {
+            echo '';
         }
-         elseif($i!=0 || $j != 0 || $k != 0){
-           echo '<hr class="my-0">';
-           echo '<li class="nav-link py-1 mx-5">' . '<h6><a href="/users/viewAll?key='.$key.'">View All</a><h6></li>';
+    }
 
-       }
-               
-            
-        }
+    if ($i == 0 && $j == 0 && $k == 0) {
+        echo '<li class="nav-link">' . '<h6 class="text-white">No Data Found<h6></li>';
+    } elseif ($i != 0 || $j != 0 || $k != 0) {
+        echo '<hr class="my-0">';
+        echo '<li class="text-center pt-3">' . '<h6><a href="/users/viewAll?key=' . $key . '">View All</a><h6></li>';
+    }
+}
